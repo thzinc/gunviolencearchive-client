86a973414bf4ea19dfc61333a346a2b2e548aaa7
Daniel James <daniel@thzinc.com>
2020-05-25 10:47:40 -0700

    (+semver: patch) Minor refactor

--- package/cmd/cli/main.go
+++ package/cmd/cli/main.go
@@ -1,8 +1,6 @@
 package cli
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -10,17 +8,13 @@ import (
 
 var (
 	log     *zap.SugaredLogger
-	debug   bool
+	verbose bool
 	rootCmd = &cobra.Command{
 		Use: "cli",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("wow")
-		},
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
 			config := zap.NewProductionConfig()
 			config.Encoding = "console"
-			if debug {
-				fmt.Println("this is debug")
+			if verbose {
 				config.Level = zap.NewAtomicLevelAt(zapcore.DebugLevel)
 			} else {
 				config.Level = zap.NewAtomicLevelAt(zapcore.WarnLevel)
@@ -31,14 +25,14 @@ var (
 	}
 )
 
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose logging")
+}
+
 // Execute runs the command line interface to the client
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-}
-
-func init() {
-	rootCmd.PersistentFlags().BoolVarP(&debug, "verbose", "v", false, "Enable verbose logging")
 }
